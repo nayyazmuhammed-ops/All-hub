077a3b187e314f879a9bdfb6b87afdbcae6a74a9
nayyazmuhammed-ops <nayyazmuhammed@gmail.com>
2026-04-27 17:11:24 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,33 +1,11 @@
---// SCRIPT LINK
-local SCRIPT_URL = "https://raw.githubusercontent.com/nayyazmuhammed-ops/All-hub/main/vziwhbduk"
+local url = "YOUR_SCRIPT_URL"
 
---// KEY SYSTEM
-local KEYS = {
-    ["ZAYYAN123"] = {expires = 9999999999}, -- permanent
-    ["FRIEND"] = {expires = os.time() + 86400}, -- 1 day
-}
-
---// GET KEY
-local key = getgenv().script_key
-
---// CHECK KEY
-local data = KEYS[key]
-
-if not data then
-    return warn("Invalid key")
-end
-
-if os.time() > data.expires then
-    return warn("Key expired")
-end
-
---// LOAD SCRIPT
-local success, script = pcall(function()
-    return game:HttpGet(SCRIPT_URL)
+local success, data = pcall(function()
+    return game:HttpGet(url)
 end)
 
 if success then
-    loadstring(script)()
+    loadstring(data)()
 else
     warn("Failed to load script")
 end
